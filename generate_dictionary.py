ab974c84b7f553297188815019a4b1d98f5430bd
jonasengelmann <jonenone+github@gmail.com>
2018-06-19 20:16:03 +0200

    first commit

---
+++ generate_dictionary.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+# coding: utf-8
+import os
+import codecs
+
+current_dir=os.getcwd()
+words=[]
+
+for file in os.listdir(current_dir):
+	if file.endswith(".txt") or file.endswith(".lab"):
+		with codecs.open(file, 'r',encoding="utf-8") as txtfile:
+			transcripttext=txtfile.read()
+
+		# Remove Punctuation 
+		transcripttext=transcripttext.replace(",","")
+		transcripttext=transcripttext.replace(".","")
+		transcripttext=transcripttext.replace("?","")
+		transcripttext=transcripttext.replace("!","")
+
+		# Remove possible double space:
+		transcripttext=transcripttext.replace("  "," ")
+
+		# Lower capital letters:
+		transcripttext=transcripttext.lower()
+
+		# Split in words and save in list:
+		words=words+transcripttext.split(" ")
+
+# Make list of words unique:
+words=list(set(words))
+
+# Sort alphabetically:
+words=sorted(words)
+
+# Save to text file:
+with codecs.open("Dictionary.txt", 'w',encoding="utf-8") as output:
+	output.write("\n".join(words))
+
+
+
+
---
+++ rule_based_phonetic_transcription.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+# coding: utf-8
+import os
+import codecs
+
+"""Simple template script that phonetically transcribes entries from
+a dictionary according to a simple rule based system. """
+
+# Dictionary filename with entries to phonetically transcribe:
+dictionary_file="./Dictionary.txt"
+
+result=[]
+
+with codecs.open(dictionary_file,"r",encoding="utf-8") as file:
+	text=file.read()
+
+# Split into words:
+words=text.split("\n")
+
+# Cycle through each word:
+for word in words:
+	single_letters=list(word)
+	length=len(single_letters)
+	phonetic_transcription=[]
+	skip_next_letter=False
+
+	# Create phonetic transcription letter by letter:
+	for idx,letter in enumerate(single_letters):
+
+		if skip_next_letter:
+			skip_next_letter=False
+			continue
+
+		#### Multiple letters:
+		## Example:
+		# ch -> x
+		if idx<length-1 and letter=="c" and single_letters[idx+1]=="h":
+			phonetic_transcription.append("x")
+			skip_next_letter=True
+
+		#### Single letters:
+		## Example:
+		# c -> ts
+		# ć -> s
+		elif letter=="c":
+			phonetic_transcription.append("ts")
+
+		elif letter=="ć":
+			phonetic_transcription.append("s")
+
+		#### Final position:
+		## Example:
+		# final b->p
+		elif letter=="b" and idx==length-1:
+			phonetic_transcription.append("p")
+
+		# If no rule, take over single letter:
+		else:
+			phonetic_transcription.append(letter)
+		
+	result.append(word+" "+" ".join(phonetic_transcription))
+
+#Save to text file:
+with codecs.open("Dictionary_with_transcription.txt", 'w',encoding="utf-8") as output:
+	output.write("\n".join(result))
+
+
+
+
