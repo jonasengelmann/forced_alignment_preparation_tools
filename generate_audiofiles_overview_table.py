4de9ffdd548145435785a80ea89b811baeb5918f
jonasengelmann <jonenone+github@gmail.com>
2018-07-04 19:48:09 +0200

    Restructed code

--- generate_audiofiles_overview_table.py
+++ generate_audiofiles_overview_table.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 # coding: utf-8
 import os
-import librosa
-import pandas as pd
 import datetime
+import fnmatch
+
+import pandas as pd
+import librosa
 
 
 def get_audio_duration(filename):
-    ''' Calculates the duration of an audio file in seconds'''
+    '''
+    Calculates the duration of an audio file in seconds.
+    Also returns samplerate.
+    '''
     # Load wav file:
     y, sr = librosa.load(filename)
     # Get duration:
@@ -15,32 +20,47 @@ def get_audio_duration(filename):
     return duration, sr
 
 
+def find_wav_file(directory=None):
+    '''
+    Runs through directory and subdirectories to return all wav files.
+    If no directory is passed, it will take the current directory.
+    '''
+    if not directory:
+        directory = os.getcwd()
+    assert os.path.isdir(directory)
+    for root, dirnames, filenames in os.walk(directory):
+        for filename in fnmatch.filter(filenames, '*.wav'):
+            yield os.path.join(root, filename)
+
+
 if __name__ == "__main__":
-    total_duration = 0
-    num_of_files = 0
-    df = pd.DataFrame(
-        columns=["Filename", "Location", "Duration [s]", "Samplerate", "Hash"])
+    total_duration, num_of_files = 0, 0
+    all_filehashes = []
+    df = pd.DataFrame()
 
-    for root, dirs, files in os.walk(os.getcwd(), topdown=False):
-        for filename in files:
-            if filename.endswith(".wav"):
-                num_of_files += 1
-                duration, samplerate = get_audio_duration(
-                    os.path.join(root, filename))
+    for filename in find_wav_file():
 
-                total_duration += duration
+        num_of_files += 1
 
-                filehash = hash(
-                    open(os.path.join(root, filename), 'rb').read())
+        duration, samplerate = get_audio_duration(filename)
 
-                df = df.append(pd.DataFrame({
-                    'Filename': filename,
-                    'Location': root,
-                    'Duration [s]': duration,
-                    'Samplerate': samplerate,
-                    'Hash': filehash}, index=[0]), sort=False)
+        total_duration += duration
+
+        # Calculate hash to find potential duplicates:
+        file_hash = hash(open(filename, 'rb').read())
+        all_filehashes.append(file_hash)
+
+        # Append information to result table:
+        df = df.append(pd.DataFrame({
+            'Filename': os.path.basename(filename),
+            'Location': os.path.dirname(filename),
+            'Duration [s]': duration,
+            'Samplerate': samplerate,
+            'Hash': file_hash}, index=[0]), sort=False)
 
     print("Number of files found = {}".format(num_of_files))
+
+    print("Number of unique files = {}".format(len(set(all_filehashes))))
 
     # Reformat total duration from seconds into days, hours, minutes
     total_duration = str(datetime.timedelta(seconds=total_duration))
